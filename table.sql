adda32200cce2b4a9be18197a3ad6b4ec0e78a39
alicealice129 <hiromi2929sui@gmail.com>
2017-05-19 02:02:52 +0900

    Update table.sql

--- table.sql
+++ table.sql
@@ -1,61 +1,5 @@
 -- DB名：cupcake
--- ①商品情報テーブル(item_master)　−>　自動販売機で販売する商品情報
-CREATE TABLE ec_item_master (
-  item_id INT(11) NOT NULL AUTO_INCREMENT COMMENT '商品ID',
-  item_name VARCHAR(255) NOT NULL COMMENT '商品名',
-  item_price INT(11) NOT NULL COMMENT '値段',
-  item_img VARCHAR(255) NOT NULL COMMENT '商品の画像ファイル名',
-  status INT(11) NOT NULL	COMMENT '公開ステータス（0:非公開、1:公開)',
-  item_stock INT(11) NOT NULL COMMENT '在庫数',
-  create_datetime DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'レコードの作成日',
-  update_datetime datetime not null default current_timestamp on update current_timestamp COMMENT 'レコードの更新日',
-  primary key(item_id)
-);
--- ②カートテーブル(ec_cart)　−>　購入予定の商品情報
-DROP TABLE ec_cart;
-CREATE TABLE ec_cart (
-  cart_id INT(11) NOT NULL AUTO_INCREMENT COMMENT 'カートID',
-  user_id INT(11) NOT NULL COMMENT 'ユーザID',
-  item_id INT(11) NOT NULL COMMENT '商品ID',
-  amount INT(11) NOT NULL COMMENT '購入予定商品数',
-  create_datetime DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'レコードの作成日',
-  update_datetime datetime not null default current_timestamp on update current_timestamp COMMENT 'レコードの更新日',
-  primary key(cart_id)
-);
--- ③ユーザテーブル(ec_user)　−>　ユーザ情報
--- user_nameとuser_emailはユニークに設定する！
-CREATE TABLE ec_user (
-  user_id INT(11) NOT NULL AUTO_INCREMENT COMMENT 'ユーザID',
-  user_name VARCHAR(255) NOT NULL COMMENT 'ユーザ名',
-  user_email VARCHAR(255) NOT NULL COMMENT 'メールアドレス',
-  user_password VARCHAR(255) NOT NULL COMMENT 'パスワード',
-  create_datetime DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'レコードの作成日',
-  update_datetime datetime not null default current_timestamp on update current_timestamp COMMENT 'レコードの更新日',
-  primary key(user_id)
-);
--- ④購入履歴テーブル(ec_item_history)　−>　商品を購入した履歴情報
--- やり直し！
-CREATE TABLE ec_item_history (
-  history_id INT(11) NOT NULL AUTO_INCREMENT COMMENT '履歴ID',
-  user_id INT(11) NOT NULL COMMENT 'ユーザID',
-  item_id INT(11) NOT NULL COMMENT '商品ID',
-  -- item_price
-  create_datetime DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'レコードの作成日',
-  primary key(history_id)
-);
--- ⑤お気に入りテーブル(ec_item_favorite)　−>　ユーザがお気に入りボタンを押すことによって挿入されたお気に入り情報
-CREATE TABLE ec_item_favorite (
-  favorite_id INT(11) NOT NULL AUTO_INCREMENT COMMENT 'お気に入りID',
-  user_id INT(11) NOT NULL COMMENT 'ユーザID',
-  item_id INT(11) NOT NULL COMMENT '商品ID',
-  create_datetime DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'レコードの作成日',
-  update_datetime datetime not null default current_timestamp on update current_timestamp COMMENT 'レコードの更新日',
-  primary key(favorite_id)
-);
-
-
--- ２回目やり直し！！
--- ①商品情報テーブル(item_master)　−>　自動販売機で販売する商品情報
+-- ①商品情報テーブル(item_master)　−>　ECサイトで販売する商品情報
 DROP TABLE ec_item_master;
 CREATE TABLE ec_item_master (
   item_id INT(11) NOT NULL AUTO_INCREMENT COMMENT '商品ID',
@@ -65,7 +9,7 @@ CREATE TABLE ec_item_master (
   status INT(11) NOT NULL	COMMENT '公開ステータス（0:非公開、1:公開)',
   item_stock INT(11) NOT NULL COMMENT '在庫数',
   create_datetime DATETIME NOT NULL COMMENT 'レコードの作成日',
-  update_datetime datetime not null COMMENT 'レコードの更新日',
+  update_datetime DATETIME NOT NULL COMMENT 'レコードの更新日',
   primary key(item_id)
 );
 -- ②カートテーブル(ec_cart)　−>　購入予定の商品情報
@@ -76,11 +20,11 @@ CREATE TABLE ec_cart (
   item_id INT(11) NOT NULL COMMENT '商品ID',
   amount INT(11) NOT NULL COMMENT '購入予定商品数',
   create_datetime DATETIME NOT NULL COMMENT 'レコードの作成日',
-  update_datetime datetime not null COMMENT 'レコードの更新日',
+  update_datetime DATETIME NOT NULL COMMENT 'レコードの更新日',
   primary key(cart_id)
 );
 -- ③ユーザテーブル(ec_user)　−>　ユーザ情報
--- user_nameとuser_emailはユニークに設定する！
+-- user_nameとuser_emailはユニークに設定
 DROP TABLE ec_user;
 CREATE TABLE ec_user (
   user_id INT(11) NOT NULL AUTO_INCREMENT COMMENT 'ユーザID',
@@ -88,7 +32,7 @@ CREATE TABLE ec_user (
   user_email VARCHAR(255) NOT NULL COMMENT 'メールアドレス',
   user_password VARCHAR(255) NOT NULL COMMENT 'パスワード',
   create_datetime DATETIME NOT NULL COMMENT 'レコードの作成日',
-  update_datetime datetime not null COMMENT 'レコードの更新日',
+  update_datetime DATETIME NOT NULL COMMENT 'レコードの更新日',
   primary key(user_id)
 );
 -- ④購入履歴テーブル(ec_item_history)　−>　商品を購入した履歴情報
@@ -99,7 +43,7 @@ CREATE TABLE ec_item_history (
   item_id INT(11) NOT NULL COMMENT '商品ID',
   item_price INT(11) NOT NULL COMMENT '値段',
   create_datetime DATETIME NOT NULL COMMENT 'レコードの作成日',
-  update_datetime datetime not null COMMENT 'レコードの更新日',
+  update_datetime DATETIME NOT NULL COMMENT 'レコードの更新日',
   primary key(history_id)
 );
 -- ⑤お気に入りテーブル(ec_item_favorite)　−>　ユーザがお気に入りボタンを押すことによって挿入されたお気に入り情報
@@ -109,6 +53,6 @@ CREATE TABLE ec_item_favorite (
   user_id INT(11) NOT NULL COMMENT 'ユーザID',
   item_id INT(11) NOT NULL COMMENT '商品ID',
   create_datetime DATETIME NOT NULL COMMENT 'レコードの作成日',
-  update_datetime datetime not null COMMENT 'レコードの更新日',
+  update_datetime DATETIME NOT NULL COMMENT 'レコードの更新日',
   primary key(favorite_id)
 );
